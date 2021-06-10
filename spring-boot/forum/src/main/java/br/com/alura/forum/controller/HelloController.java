1b19a7a1dba74b95e4d6c99de775b7935e6185c3
gabrielnp <gabrielnp.2010@gmail.com>
2021-06-10 10:48:05 -0300

    feat: add repository interface to Topico
    CHANGE LOG
    access data in db to be returned

--- spring-boot/forum/src/main/java/br/com/alura/forum/controller/HelloController.java
+++ spring-boot/forum/src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -1,15 +1,14 @@
 package br.com.alura.forum.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String hello() {
-        return "hellor world";
+        return "hello world";
     }
     
 }
--- spring-boot/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,25 +1,25 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista() {
-        Topico topico = new Topico("Duvida", "Duvida com Stpring", new Curso("Spring", "Programacao"));
-
-        return TopicoDto.Converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.Converter(topicos);
     }
     
 }
