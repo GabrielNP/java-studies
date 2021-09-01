dbf9c30408f79e17157080281ef7e64d88dece56
gabrielnp <gabrielnp.2010@gmail.com>
2021-09-01 18:53:58 -0300

    feat: authenticate by jwt
    CHANGE LOG:
    retrieve and validate token sent on requests
    add filter that work as middleware to intercept requests and validate token

--- spring-boot/forum/src/main/java/br/com/forum/config/security/TokenService.java
+++ spring-boot/forum/src/main/java/br/com/forum/config/security/TokenService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.forum.model.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -30,5 +31,19 @@ public class TokenService {
                     .setExpiration(expirationDate)
                     .signWith(SignatureAlgorithm.HS256, secret)
                     .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
     }
 }
